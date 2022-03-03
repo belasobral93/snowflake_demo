44c3796e949599596e727467f24ae3a832ddc454
Isabela Sobral <isabela.sobral@dbtlabs.com>
2022-03-03 06:28:39 +0000

    test slim ci

---
+++ macros/codegen.sql
@@ -0,0 +1,16 @@
+
+{{ codegen.generate_model_yaml(
+    model_name='stg_tpch_customers'
+) }}
+
+{{ codegen.generate_base_model(
+    source_name='tpch',
+    table_name='orders',
+    leading_commas = false
+) }}
+
+{{ codegen.generate_source(
+    schema_name = 'tpch_sf001',
+    generate_columns = true
+) }}
+
