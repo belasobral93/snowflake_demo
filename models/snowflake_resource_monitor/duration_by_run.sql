4eff2b0c526a52e859f4e49f3ae83554e621f365
Isabela Sobral <isabela.sobral@dbtlabs.com>
2022-09-20 00:03:14 +0000

    b

--- models/snowflake_resource_monitor/duration_by_run.sql
+++ models/snowflake_resource_monitor/duration_by_run.sql
@@ -3,3 +3,5 @@ from {{ ref('inc_query_logs') }}
 where dbt_run_id = '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}'
 group by 1
 order by dbt_run_id desc
+
+{{ log("RUN ID IS " + '{{ env_var("DBT_CLOUD_RUN_ID"}}', info = true) }}
