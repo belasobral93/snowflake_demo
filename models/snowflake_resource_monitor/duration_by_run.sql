10dbbd55e585d574d840b534c82edbdcf72b9f98
Isabela Sobral <isabela.sobral@dbtlabs.com>
2022-09-20 01:43:56 +0000

    added test for run duration

--- models/snowflake_resource_monitor/duration_by_run.sql
+++ models/snowflake_resource_monitor/duration_by_run.sql
@@ -1,9 +1,6 @@
-select dbt_run_id, sum(total_elapsed_time) as total_run_durationz
+select dbt_run_id, sum(total_elapsed_time) as total_run_duration
 from {{ ref('inc_query_logs') }}
-where dbt_run_id = '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}'
+-- where dbt_run_id = '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}'
 group by 1
 order by dbt_run_id desc
 
-{%- set central_tbl -%} '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}' {%- endset -%}
-
-{{ log(central_tbl, info = true) }}
